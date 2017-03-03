22384350adbcc50050b0ecbe17f3468950956e7d
Magesh <tkmagesh77@gmail.com>
2017-03-03 16:09:11 +0530

    Day-03 Complete

--- Day-03/03-Async/async.js
+++ Day-03/03-Async/async.js
@@ -12,23 +12,57 @@ var pgm = (function(){
 		console.log(`[Client] result = ${result}`);
 	}
 
-	function addAsync(x,y){
+	function addAsync(x,y, onResult){
 		console.log(`		[Service] processing ${x}, ${y}`);
 		setTimeout(function(){
 			var result = x + y;
 			console.log(`		[Service] returning result`);
-			return result;
+			if (typeof onResult === 'function')
+				onResult(result);
 		},5000);
 	}
 
 	function addAsyncClient(x,y){
 		console.log(`[Client] trigger addAsync`);
-		var result = addAsync(x,y);
-		console.log(`[Client] result = ${result}`);
+		addAsync(x,y, function(result){
+			console.log(`[Client] result = ${result}`);
+		});
 	}
 
+	var addAsyncEvents = (function(){
+		var _subscribers = [];
+		function subscribe(subscriptionFn){
+			_subscribers.push(subscriptionFn);
+		}
+		function add(x,y){
+			console.log(`		[Service] processing ${x}, ${y}`);
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`		[Service] returning result`);
+				_subscribers.forEach(subscriber => subscriber(result));
+			},5000);
+		}
+		return {
+			add : add,
+			subscribe : subscribe
+		}
+	})()
+
+	function addAsyncPromise(x,y){
+		var promise = new Promise(function(resolveFn, rejectFn){
+			console.log(`		[Service] processing ${x}, ${y}`);
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`		[Service] returning result`);
+				resolveFn(result);
+			},5000);
+		});
+		return promise;
+	}
 	return {
 		addSyncClient : addSyncClient,
-		addAsyncClient : addAsyncClient
+		addAsyncClient : addAsyncClient,
+		addAsyncEvents : addAsyncEvents,
+		addAsyncPromise : addAsyncPromise
 	};
 })();
