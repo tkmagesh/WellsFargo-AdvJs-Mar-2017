384a81c9f67cb336a054a1aef1e9e0991ffc86eb
Magesh <tkmagesh77@gmail.com>
2017-03-03 10:50:37 +0530

    radio.js

---
+++ Day-03/01-pubsub/radio.js
@@ -0,0 +1,46 @@
+
+var radio = (function(){
+	var contexts = {
+		
+	}
+
+	function radio(eventName){
+		if (!contexts[eventName])
+			contexts[eventName] = new Context(eventName);
+		return contexts[eventName];
+	}
+
+	function Context(eventName){
+		this.__eventName  = eventName;
+		this.__subscribers = [];
+	}
+
+	Context.prototype.subscribe = function(){
+		Array.prototype.forEach.call(arguments, function(subscription){
+			if (Array.isArray(subscription)){
+				this.__subscribers.push(subscription[0].bind(subscription[1]))
+			} else if (typeof subscription === 'function'){
+				this.__subscribers.push(subscription)	
+			}
+		})
+		return this;
+	}
+
+	Context.prototype.unsubscribe = function unsubscribe(subscriptionFn){
+		this.__subscribers = this.__subscribers.filter(function(subscriber){
+			return subscriber !== subscriptionFn;
+		});
+		return this;
+	}
+
+	Context.prototype.broadcast = function broadcast(){
+		var args = arguments;
+		this.__subscribers.forEach(function(subscriptionFn){
+			subscriptionFn.apply(this, args);
+		});
+		return this;
+	}
+
+	return radio;
+})()
+
